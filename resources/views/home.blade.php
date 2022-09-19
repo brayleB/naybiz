96ebd72d399ab1beaed27015db94df82f33b55d9
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-09-19 11:39:13 +0800

    haha

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>	
 	<title>Naybiz</title>
-    
+	@vite('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap')
+    @vite('resources/css/bootstrap.min.css')
+	@vite('resources/css/fontawesome-all.min.css')
+	@vite('resources/css/swiper.css')
+	@vite('resources/css/styles.css')
 	@vite('resources/css/app.css')
 </head>
 <body>
 	<div id="app">    
     </div>
 	@vite('resources/js/app.js')
+	@vite('resources/js/imports/bootstrap.min.js')
+	@vite('resources/js/imports/swiper.min.js')
+	@vite('resources/js/imports/scripts.js')
 </body>
 </html>
