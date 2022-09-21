dea6cff90a7d8b1409d1dbf11f27a96126d127c0
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-09-21 22:52:40 +0800

    update-css issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,5 @@
 import './bootstrap';
-import '../css/app.css'; 
-import '../css/bootstrap.min.css'; 
-import '../css/fontawesome-all.min.css'; 
-import '../css/swiper.css'; 
-import '../css/style.css'; 
+
 import '../js/imports/bootstrap.min.js';
 import '../js/imports/swiper.min.js';
 import '../js/imports/scripts.js';
