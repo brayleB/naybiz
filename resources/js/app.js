d363d7b362b75286bb74f660b49bb758cfb64138
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-09-21 22:46:00 +0800

    update - vite issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import '../css/bootstrap.min.css';
 import '../css/fontawesome-all.min.css'; 
 import '../css/swiper.css'; 
 import '../css/style.css'; 
+import '../js/imports/bootstrap.min.js';
+import '../js/imports/swiper.min.js';
+import '../js/imports/scripts.js';
 import { createApp } from 'vue';  
 import App from './components/main.vue' 
 createApp(App).mount("#app")
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,11 @@
 	   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">	   			  
 	
 	<title>Naybiz</title>
-	{{-- @vite('resources/css/app.css')
-    @vite('resources/css/bootstrap.min.css')
-	@vite('resources/css/fontawesome-all.min.css')
-	@vite('resources/css/swiper.css')
-	@vite('resources/css/styles.css') --}}
-
+  
 </head>
 <body>
 	<div id="app">    
     </div>
 	@vite('resources/js/app.js')
-	@vite('resources/js/imports/bootstrap.min.js')
-	@vite('resources/js/imports/swiper.min.js')
-	@vite('resources/js/imports/scripts.js')
 </body>
 </html>
