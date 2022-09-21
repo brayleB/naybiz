ff98813bffb80d04d190db6e6c0a4db6c498d2c7
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-09-21 22:56:02 +0800

    update - fix css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
 	   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">	   			  
 	
 	<title>Naybiz</title>
+	@vite('resources/css/app.css')
   
 </head>
 <body>
