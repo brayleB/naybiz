788782f54fa8f111b7bef178fc2e571cea3b6d12
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-10-11 15:47:13 +0800

    Api route

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware'=>['auth:sanctum']], function(){
     Route::post('/auth/logout',[AuthController::class,'logout']);
+    Route::post('/properties/add', [AuthController::class, 'addProperties']);
 });
