aebd5eb3a897a9c1009dd11669e46bed29869d21
Brayle Bautista <braylebautista22@gmail.com>
2022-10-12 14:28:46 +0800

    Update for table components fix

--- resources/js/components/tabs/tabnav.vue
+++ resources/js/components/tabs/tabnav.vue
@@ -1,14 +1,17 @@
-<template>
-   
-                            
-        <ul class="nav nav-tabs">
-            <li v-for="tab in tabs" :key="tab" class="nav-item">                    
-                <button class="nav-link" :class="{ active: tab === selected}" @click="setTab(tab)">
-                    {{ tab }}
-                </button>
-            </li>
-        </ul>
-                             
+<template>      
+    <div class="con container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">                               
+                <ul class="nav nav-tabs">
+                    <li v-for="tab in tabs" :key="tab" class="nav-item">                    
+                        <button type="button" class="nav-link" :class="{ active: tab === selected}" @click="setTab(tab)">
+                            {{ tab }}
+                        </button>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>                                                                                      
         <slot>
         </slot>    
 </template>
@@ -35,5 +38,7 @@
 </script>
 
 <style scoped>
-
+.con {
+    margin-top: 1.5%;
+}
 </style>
