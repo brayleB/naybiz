32c6cc44bf2af4d514bec5f66cf5ec55bafd975d
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-10-19 18:57:23 +0800

    additionals route

--- resources/js/components/modal/modal.vue
+++ resources/js/components/modal/modal.vue
@@ -1,12 +1,23 @@
 <template>
-   <div>
-  <b-modal id="modal-details" hide-header>
-    <div class="d-block">
-      <h1>{{ tenant_details.firstname }}</h1>
-      <h3>{{ tenant_details.about }}</h3>
+    <div class="modal fade" id="myModal" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">{{ tenants_list_new.first_name }}</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
     </div>
-  </b-modal>
-</div>
 </template>
 <script>
 export default {
